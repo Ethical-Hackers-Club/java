536b43758c8c1add2032185664243908e8bcb345
Shivam Goswami <shivam.i.gosavi@live.com>
2017-05-06 23:13:41 -0700

    Add files via upload

---
+++ shivam.js
@@ -0,0 +1,8 @@
+$(document).ready(function()
+{
+  var aa=$("#designercredits").val();
+  if (aa == null) {
+    window.location.href = "http://www.techgoogle.org/";
+  };
+ $("#designercredits").attr("href","http://www.techgoogle.org/");
+});
